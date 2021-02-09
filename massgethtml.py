400dfabc1cb886467bfa82ffc51930dafd1c1e68
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2021-02-09 14:54:48 -0500

    Changes

--- massgethtml.py
+++ massgethtml.py
@@ -1,2 +1,28 @@
 from requests import get
+from configuration import REST_ENDPOINT_URL, SCRAP_DIR
+from mw_api_client import Wiki
+import os
+
+wiki = Wiki('https://snapwiki.miraheze.org/w/api.php', 'Snap! Wiki Scrapper/1.0.0')
+pages = wiki.allpages()
+
+for page in pages:
+
+    filename1 = SCRAP_DIR + page.title
+    try:
+        if filename1.count('/') > 1:
+            for dir in filename1.split('/'):
+                if not dir == '/':
+                    path = SCRAP_DIR + dir
+                    if not os.path.isdir(path):
+                        os.mkdir(path)
+        filename = open(filename1 + '.html', 'w', encoding='utf-8')
+        restfilepath = REST_ENDPOINT_URL + page.title
+        print(restfilepath)
+        print(filename1)
+        filename.write(get(restfilepath).text)
+        filename.close()
+
+    except:
+        print("An exception occurred")
 
