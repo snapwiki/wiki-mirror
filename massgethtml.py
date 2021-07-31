a3642d4bea659a3f7dea9a504d7119de38816eff
R4356th <61620631+R4356th@users.noreply.github.com>
2021-07-31 19:53:06 +0600

    Bump ScratchBlocks

--- massgethtml.py
+++ massgethtml.py
@@ -20,7 +20,7 @@ for page in pages:
                             os.mkdir(path)
             filename = open(filename1 + '.html', 'w', encoding='utf-8')
             restfilepath = REST_ENDPOINT_URL + page.title
-            filename.write(get(restfilepath).text.replace('</body>', '<script src="https://scratchblocks.github.io/js/scratchblocks-v3.5-min.js"></script><script>scratchblocks.renderMatching(`.blocks`, {});</script></body>'))
+            filename.write(get(restfilepath).text.replace('</body>', '<script src="https://scratchblocks.github.io/js/scratchblocks-v3.5.2-min.js"></script><script>scratchblocks.renderMatching(`.blocks`, {});</script></body>'))
             filename.close()
         except Exception as err:
             print("An exception occurred" + str(err))
