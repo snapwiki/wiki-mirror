8c40b109b9f7492c4bd4f0e6cfcad3cb4112390a
R4356th <61620631+R4356th@users.noreply.github.com>
2021-08-10 11:21:40 +0600

    Resume scraping subpages as ?action=render supports them

--- massgethtml.py
+++ massgethtml.py
@@ -7,23 +7,22 @@ wiki = Wiki(API_URL, USERAGENT)
 pages = wiki.allpages()
 indexslot = ''
 for page in pages:
-    if not '/' in page.title:
-        try:
-            generatedslot = '<tr><td class="px-6 py-4 whitespace-nowrap text-left text-sm font-medium">' + page.title + '</td><td class="px-6 py-4 whitespace-nowrap text-left text-sm font-medium"><a class="text-indigo-500 hover:text-indigo-600" href="' + page.title + '.html">' + 'Visit</a></td></tr>'
-            indexslot += generatedslot
-            filename1 = SCRAP_DIR + page.title
-            if filename1.count('/') > 1:
-                for directory in filename1.split('/'):
-                    if not directory == '/':
-                        path = SCRAP_DIR + directory
-                        if not os.path.isdir(path):
-                            os.mkdir(path)
-            filename = open(filename1 + '.html', 'w', encoding='utf-8')
-            articleurl = WIKI_ARTICLE_PATH + page.title + '?action=render'
-            filename.write(get(articleurl).text.replace('</body>', '<script src="https://scratchblocks.github.io/js/scratchblocks-v3.5.2-min.js"></script><script>scratchblocks.renderMatching(`.blocks`, {});</script></body>'))
-            filename.close()
-        except Exception as err:
-            print("An exception occurred" + str(err))
+    try:
+        generatedslot = '<tr><td class="px-6 py-4 whitespace-nowrap text-left text-sm font-medium">' + page.title + '</td><td class="px-6 py-4 whitespace-nowrap text-left text-sm font-medium"><a class="text-indigo-500 hover:text-indigo-600" href="' + page.title + '.html">' + 'Visit</a></td></tr>'
+        indexslot += generatedslot
+        filename1 = SCRAP_DIR + page.title
+        if filename1.count('/') > 1:
+            for directory in filename1.split('/'):
+                if not directory == '/':
+                    path = SCRAP_DIR + directory
+                    if not os.path.isdir(path):
+                       os.mkdir(path)
+        filename = open(filename1 + '.html', 'w', encoding='utf-8')
+        articleurl = WIKI_ARTICLE_PATH + page.title + '?action=render'
+        filename.write(get(articleurl).text.replace('</body>', '<script src="https://scratchblocks.github.io/js/scratchblocks-v3.5.2-min.js"></script><script>scratchblocks.renderMatching(`.blocks`, {});</script></body>'))
+        filename.close()
+     except Exception as err:
+        print("An exception occurred" + str(err))
 
 index = open(SCRAP_DIR + '/index.html', 'w', encoding='utf-8')
 index.write(INDEX_HEAD + indexslot + INDEX_BODY)
